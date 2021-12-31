0621c605d76fd80f21d36b40b93152d488507197
danish <irdanish11@gmail.com>
2021-12-31 23:28:35 +0500

    added youtube viewer and upwork script

---
+++ misc/pipe-viewer.sh
@@ -0,0 +1,19 @@
+# make sure mpv is already installed
+cd ~/Downloads
+# install youtube-dl
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
+# clone pipe viewer repo
+git clone git@github.com:trizen/pipe-viewer.git
+cd pipe-viewer
+
+# install the build essentials
+sudo apt install build-essential
+# configuring Build Module 
+# cpan
+cpan Module::Build
+
+# installation
+perl Build.PL
+sudo ./Build installdeps
+sudo ./Build install

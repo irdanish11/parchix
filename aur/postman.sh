a4c9b5b40bf8d86e4882ee3db786ae931fcf73c9
danish <irdanish11@gmail.com>
2021-12-31 11:32:22 +0500

    added package installation from aur and some other resources

---
+++ aur/postman.sh
@@ -0,0 +1,10 @@
+cd ~/Downloads
+
+git clone https://aur.archlinux.org/postman-bin.git
+cd postman-bin
+
+makepkg -sir --noconfirm
+
+sudo pacman -U postman-bin-*.pkg.tar.zst --noconfirm
+
+cd ../ && sudo rm -rfv postman-bin/
